0833f6e91479f05e07977540383e5e3e939bd118
DFoudi <david.foudi1@gmail.com>
2023-05-12 03:12:40 +0200

    REST API Development Best Practices. Automapper and Repositories

---
+++ HotelListing.API/Configurations/AutoMapperConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using HotelListing.API.Data;
+using HotelListing.API.Models.Country;
+using HotelListing.API.Models.Hotels;
+
+namespace HotelListing.API.Configurations
+{
+    public class AutoMapperConfig : Profile
+    {
+        public AutoMapperConfig()
+        {
+            // Country Mappings
+            CreateMap<Country, CreateCountryDto>().ReverseMap();
+            CreateMap<Country, GetCountryDto>().ReverseMap();
+            CreateMap<Country, CountryDto>().ReverseMap();
+            CreateMap<Country, UpdateCountryDto>().ReverseMap();
+
+            // Hotel Mappings
+            CreateMap<Hotel, HotelDto>().ReverseMap();
+        }
+    }
+}
